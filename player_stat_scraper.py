7361d6495aa704044bb882fc9d049a5b16af4f02
Josh Wasserman <joshwasserman@Joshs-MacBook-Air-2.local>
2018-09-14 00:36:27 -0500

    removed deprecated file

--- player_stat_scraper.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-
-url = 'https://www.basketball-reference.com/players/d/duranke01.html'
-response = requests.get(url).text
-
-print(response)
